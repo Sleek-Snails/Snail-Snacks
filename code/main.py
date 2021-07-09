880c170f3138dd40e35497084f67239b9c808ee3
Sam Ryan <r.sam.ryan@gmail.com>
2021-07-09 16:12:44 -0400

    Switched to pynput & added example

--- code/main.py
+++ code/main.py
@@ -1,1 +1,44 @@
-print("Hello World!")
+import time
+
+from pynput import keyboard
+# from rich.color import Color #noqa: F401
+from rich.live import Live
+# from rich.style import Style #noqa: F401
+# from rich.live import refresh #noqa: F401
+from rich.text import Text
+
+# from random import randint #noqa: F401
+
+text = Text("Hello World!")
+with Live(text, auto_refresh=False) as live:
+    def on_press(key: keyboard) -> None:
+        """Event handler for keyboard on_press event"""
+        try:
+            # print('alphanumeric key {0} pressed'.format(
+            #     key.char))
+            t = Text(f"Key pressed down: {key.char}")
+            live.update(t, refresh=True)
+        except AttributeError:
+            t = Text(f"Key pressed down: {key}")
+            live.update(t, refresh=True)
+            # print('special key {0} pressed'.format(
+            #     key))
+
+    def on_release(key: keyboard) -> bool:
+        """Event handler for keyboard on_release event"""
+        # print('{0} released'.format(
+        #     key))
+        t = Text(f"Key pressed up: {key.char}")
+        live.update(t, refresh=True)
+        if key == keyboard.Key.esc:
+            # Stop listener
+            return False
+
+    listener = keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release,
+        suppress=True)
+    listener.start()
+
+    while True:
+        time.sleep(1)
