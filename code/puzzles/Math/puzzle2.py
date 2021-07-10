e97cd275d3f1b3636c928bdf77e49990768c196b
rlp81 <58034254+rlp81@users.noreply.github.com>
2021-07-09 19:19:35 -0500

    Update puzzle2.py

--- code/puzzles/Math/puzzle2.py
+++ code/puzzles/Math/puzzle2.py
@@ -1,25 +1,23 @@
 from random import randint
-num1 = randint(1,10)
-num2 = randint(1,10)
-num3 = randint(1,10)
-num4 = randint(1,2)
-ans = num1/num2
-wro = num1/num3
 from rich import print
-from rich.prompt import Prompt
 from rich.console import RenderGroup
 from rich.panel import Panel
+num1 = randint(1, 10)
+num2 = randint(1, 10)
+num3 = randint(1, 10)
+num4 = randint(1, 2)
+ans = num1/num2
+wro = num1/num3
 if num4 == 1:
     panel_group = RenderGroup(
         Panel(f"What is {num1} / {num2}?"),
         Panel(f"A - {ans}", style="on blue"),
-        Panel(f"B - {wro}", style="on red")
-        
+        Panel(f"B - {wro}", style="on red")     
     )
 if num4 == 2:
     panel_group = RenderGroup(
-        Panel(f"What is {num1} / {num2}?"),
-        Panel(f"A - {wro}", style="on blue"),
+        Panel(f"What is {num1} / {num2}?"), 
+        Panel(f"A - {wro}", style="on blue"), 
         Panel(f"B - {ans}", style="on red")
         
     )
@@ -30,26 +28,25 @@ def on_press(key):
     pass
 
 def on_release(key):
-    #Add your code to stop motor
-    if 'char' in dir(key):   #check if char method exists,
+    if 'char' in dir(key):
         if num4 == 1:
             if key.char == 'a':
                 return False
+
             if key.char == 'b':
                 print("Wrong!")
+
         if num4 == 2:
             if key.char == 'a':
                 print("Wrong!")
+                
             if key.char == 'b':
                 return False
 
-# Collect events until released
 with keyboard.Listener(
         on_press=on_press,
         on_release=on_release) as listener:
     listener.join()
-
-# ...or, in a non-blocking fashion:
 listener = keyboard.Listener(
     on_press=on_press,
     on_release=on_release)
